5187344941996ba526b24db9629959e7862c3bff
vik <v.lateralus@gmail.com>
2017-10-15 22:45:40 +0300

    Update RecoveryChooser.js

--- RecoveryChooser.js
+++ RecoveryChooser.js
@@ -2,12 +2,25 @@ const golos = require('golos-js')
 golos.config.set('websocket', "wss://api.golos.cf")
 
 const login = "lorem" // your login
-const wif= "5ouriuruemACTIVEKEYkjnfjnvnjfnvjnfjnvfjnvnjdu" // your OWNER key (match login)
+const pass = ""// your master password
+
 const newRecoveryLogin = "golosio" // Login of your newest recovery account
 
-golos.broadcast.changeRecoveryAccount(wif,login,newRecoveryLogin, "", (err, result)=> {
+let wif = golos.auth.getPrivateKeys(login, pass, ["owner","active","posting","memo"]);
+
+
+golos.broadcast.changeRecoveryAccount(wif.owner,login,newRecoveryLogin, "", (err, result)=> {
 if (err) return console.log(err);
 console.log(`Done!
 
-New recovery is ${newRecoveryLogin}`)
+New recovery is ${newRecoveryLogin}
+
+Keys:
+${pass} - Master password
+${wif.owner} - OWNER
+${wif.active} - ACTIVE
+${wif.posting} - POSTING
+${wif.memo} - MEMO
+
+`)
 });
